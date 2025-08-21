34e65de20a0548a161db10415d094564b27842ff
yasmine <bouchamiyassssmin@gmail.com>
2025-08-21 21:44:42 +0100

    securiser le mot de passe /using bcrypt

--- models/User.js
+++ models/User.js
@@ -1,9 +1,11 @@
 
 const mongoose = require("mongoose");
+const bcrypt=require('bcrypt')
+
 const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true, required: true },
-  passwordHash: { type: String, required: true },
-  displayName: String,
+  email: { type: String, unique: true, required: true,match:[/^\S+@\S+\.\S+$/, 'Please provide a valid email'] },
+  password:{type:String,required:true,minlength:6},
+  username: String,
   role: { type: String, enum: ["USER", "ADMIN"], default: "USER" },
   status: { type: Boolean, default: true },
   createdAt: { type: Date, default: Date.now },
@@ -23,5 +25,19 @@ const userSchema = new mongoose.Schema({
     objectif: { type: String, enum: ["Perte", "Maintien", "Prise"] },
   },
 });
+userSchema.pre('save',async function(next) {
+    try{
+        const salt=await bcrypt.genSalt()
+        const User=this
+        User.password=await bcrypt.hash(User.password,salt)
+        User.statu=false
+
+
+    }catch(error){
+        next(error)
+
+    }
+    
+})
 module.exports = mongoose.model("User", userSchema);
 
